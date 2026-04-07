9119afb238d30a215cbdd4a983311df452312da3
Ruben Berenguel Montoro <ruben@mostlymaths.net>
2026-04-07 15:34:07 +0200

    dd

--- tests/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ruben/code/foowrite-core2/tests
-# Build directory: /Users/ruben/code/foowrite-core2/tests/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/ruben/code/foowrite-core2/tests/build/foowrite_tests[1]_include.cmake")
-subdirs("_deps/googletest-build")
